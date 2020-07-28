9b18669ded24903e35005d1ff706a859cf20d2e5
wgcairui <wgcairui@icloud.com>
2020-07-28 08:09:24 +0800

    add

--- server/bin/UartDataParsingSave.ts
+++ server/bin/UartDataParsingSave.ts
@@ -5,15 +5,13 @@ import { LogUseBytes } from "../mongoose/Log";
 import { queryResult, queryResultArgument } from "uart";
 
 export default async (queryResultArray: queryResult[]) => {
-
-
     if (queryResultArray.length > 0) {
         // 保存每个终端使用的数字节数
         SaveTerminaluseBytes(queryResultArray)
+        // 保存原始数据
+        TerminalClientResults.insertMany(queryResultArray);
         // 翻转结果数组,已新的结果为准
         const UartData = queryResultArray.reverse()
-        // 保存原始数据
-        TerminalClientResults.insertMany(UartData);
         // 解析结果
         const ParseData = await Promise.all(UartData.map(el => ProtocolPares(el)))
             .then(el => el.filter(el2 => el2.result?.length as number > 0))
@@ -41,7 +39,7 @@ export default async (queryResultArray: queryResult[]) => {
         })
     }
 }
-
+// 保存每个查询指令使用的字节，以天为单位
 async function SaveTerminaluseBytes(queryResult: queryResult[]) {
     const date = new Date().toLocaleDateString()
     for (let el of queryResult) {
